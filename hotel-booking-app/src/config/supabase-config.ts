b87138220a55cfefc2f511ead9c8c681158179c5
Akhilesh Shende <akhilesh@skynetixsystems.com>
2026-06-05 19:42:49 +0530

    fixed registration

--- hotel-booking-app/src/config/supabase-config.ts
+++ hotel-booking-app/src/config/supabase-config.ts
@@ -1,7 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_PROJECT_URL
-const supabaseKey = process.env.SUPABASE_API_KEY
-const supabaseConfig = createClient(supabaseUrl!, supabaseKey!)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+const supabaseConfig = createClient(supabaseUrl!, supabaseKey!);
 
-export default supabaseConfig
+export default supabaseConfig;
